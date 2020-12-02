cfbeada579b9e43331b26af0f2bb41264a6e3438
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-02 18:46:52 -0300

    Elementos de React

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>¡Hola, Mundo! </p>
+        <p>:)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
