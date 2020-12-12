7332e38f80cec511c60078bbb99377ef30204ada
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-11 22:06:05 -0300

    Counter Element

--- src/Counter.js
+++ src/Counter.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function ItemCount({stock}){
-    const [numero, setNumero] = React.useState(1);
+function ItemCount(){
+    const initial = 0;
+    const [number, setNumber] = React.useState(initial);
+    const [stock] = useState(10); 
+    const [limit] = useState(0);
 
-    function sumar(){
-        setNumero(numero + 1);
+
+    function more(){
+        if(number < stock){
+            
+        setNumber(number + 1);
+        return;
     }
+}
 
-    function restar(){
-        setNumero(numero - 1);
+    function less(){
+        if(number > limit){
+        setNumber(number - 1);
     }
+}
 
     return(
+        <>
         <div>
-            <button onClick={sumar}>
-                Sumar 
-            </button>
-            <button onClick={restar}>
-                Restar 
-            </button>
-            <p>Cantidad de clicks: {numero}</p>
+            <button onClick={more}>+</button>
+            <p>Cantidad de clicks: {number}</p>
+            <button onClick={less}>-</button>
         </div>
+        <div>
+            <button>Agregar al Carrito</button>
+        </div>
+        </>
     )
 }
 
