f99e3fbaa8e58278b7ee9ae71a3d2eff5fa92b1f
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-11 22:18:22 -0300

    Nuevo nombre del archivo Item Count

---
+++ src/Components/ItemCount/ItemCount.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+function ItemCount(){
+    const initial = 0;
+    const [number, setNumber] = React.useState(initial);
+    const [stock] = useState(10); 
+    const [limit] = useState(0);
+
+
+    function more(){
+        if(number < stock){
+            
+        setNumber(number + 1);
+        return;
+    }
+}
+
+    function less(){
+        if(number > limit){
+        setNumber(number - 1);
+        return;
+    }
+}
+
+    return(
+        <>
+        <div>
+            <button onClick={more}>+</button>
+            <p>Cantidad de clicks: {number}</p>
+            <button onClick={less}>-</button>
+        </div>
+        <div>
+            <button>Agregar al Carrito</button>
+        </div>
+        </>
+    )
+}
+
+export default ItemCount;
