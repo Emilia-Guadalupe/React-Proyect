c6630be637e6ad61945493121ef1fd612ae04c9f
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-17 21:52:17 -0300

    Modifiqué detalles del import

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,6 @@
 import React from 'react';
-//import './ItemListContainer.css';
+import './ItemListContainer.css';
 import ItemList from '../ItemDetails/ItemList';
-import ItemCount from '../ItemCount/ItemCount';
-
 
 const ItemListContainer = () => {
     return (
