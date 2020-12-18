d08a6b42cb05013bded4e82c1ac3b41f44c3c677
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-17 21:50:11 -0300

    Modifiqué el contenido del componente y los estilos

--- src/Components/ItemDetails/ItemList.js
+++ src/Components/ItemDetails/ItemList.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { ItemDetails } from './Item';
+import './ItemList.css';
 
 function ItemList(){
     return(
         <>
-            {ItemDetails.map((item, index) => {
+            {ItemDetails.map((item) => {
                 return(
-                        <article key={index}>
+                        <div key={item.id} id="itemContainer">
                                 <div>
-                                <img src={item.img} alt="Portada"/>
+                                <img src={item.img} alt="Portada" />
                                 </div>
                                 <div>
                                 <h2>{item.title}</h2>
                                 <p>{item.author}</p>
                                 <p>{item.price}</p>
                                 </div>
-                        </article>
+                        </div>
                 )
-            })}
+            })
+            }
         </>
     )
 }
