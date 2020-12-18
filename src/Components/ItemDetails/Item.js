3b2c6898e6e506ec8a31a4ba653c3b5eb214df93
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-17 21:48:55 -0300

    Hice modificaciones en el array

--- src/Components/ItemDetails/Item.js
+++ src/Components/ItemDetails/Item.js
@@ -1,61 +1,62 @@
+
 export const ItemDetails = [
     {
-        "id": 0,
-        "title": "The Lost Hero",
-        "author": "Rick Riordan",
-        "price": 890,
-        "img": require('./Imagenes/The_Lost_Hero.jpg'),
-        "language": "Inglés",
-        "publisher": "Disney-Hyperion"
+        id: 0,
+        title: "The Lost Hero",
+        author: "Rick Riordan",
+        price: 890,
+        img: require('../Imagenes/The_Lost_Hero.jpg'),
+        language: "Inglés",
+        publisher: "Disney-Hyperion"
     },
     
     {
-        "id": 1,
-        "title": "Mistborn",
-        "author": "Brandon Sanderson",
-        "price": 800,
-        "img": require('./Imagenes/Mistborn_Trilogy.jpg'),
-        "language": "Inglés",
-        "publisher": "Tor Books"
+        id: 1,
+        title: "Mistborn",
+        author: "Brandon Sanderson",
+        price: 800,
+        img: require('../Imagenes/Mistborn_Trilogy.jpg'),
+        language: "Inglés",
+        publisher: "Tor Books"
     },
     
     {
-        "id": 2,
-        "title": "Stormfront",
-        "author": "Jim Butcher",
-        "price": 700,
-        "img": require('./Imagenes/Storm_Front.jpg'),
-        "language": "Inglés",
-        "publisher": "Penguin Books"
+        id: 2,
+        title: "Stormfront",
+        author: "Jim Butcher",
+        price: 700,
+        img: require('../Imagenes/Storm_Front.jpg'),
+        language: "Inglés",
+        publisher: "Penguin Books"
     },
     
     {
-        "id": 3,
-        "title": "Inkheart",
-        "author": "Cornelia Funke",
-        "price": 900,
-        "img": require('./Imagenes/Inkheart.jpg'),
-        "language": "Inglés",
-        "publisher": "Scholastic"
+        id: 3,
+        title: "Inkheart",
+        author: "Cornelia Funke",
+        price: 900,
+        img: require('../Imagenes/Inkheart.jpg'),
+        language: "Inglés",
+        publisher: "Scholastic"
     },
     
     {
-        "id": 4,
-        "title": "Six of Crows",
-        "author": "Leigh Bardugo",
-        "price": 1000,
-        "img": require('./Imagenes/Six_Of_Crows.jpg'),
-        "language": "Inglés",
-        "publisher": "Henry Holt and Co"
+        id: 4,
+        title: "Six of Crows",
+        author: "Leigh Bardugo",
+        price: 1000,
+        img: require('../Imagenes/Six_Of_Crows.jpg'),
+        language: "Inglés",
+        publisher: "Henry Holt and Co"
     },
     
     {
-        "id": 5,
-        "title": "The Raven Boys",
-        "author": "Maggie Stiefvater",
-        "price": 750,
-        "img": require('./Imagenes/The_Raven_Cycle.jpg'),
-        "language": "Inglés",
-        "publisher": "Scholastic"
+        id: 5,
+        title: "The Raven Boys",
+        author: "Maggie Stiefvater",
+        price: 750,
+        img: require('../Imagenes/The_Raven_Cycle.jpg'),
+        language: "Inglés",
+        publisher: "Scholastic"
     }
 ]
