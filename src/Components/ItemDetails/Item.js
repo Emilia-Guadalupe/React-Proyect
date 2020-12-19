e582dbbf839df7e01b191b50414dee8a721b9bdf
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-19 14:08:56 -0300

    Modifiqué el src de las imágenes

--- src/Components/ItemDetails/Item.js
+++ src/Components/ItemDetails/Item.js
@@ -5,7 +5,7 @@ export const ItemDetails = [
         title: "The Lost Hero",
         author: "Rick Riordan",
         price: 890,
-        img: require('../Imagenes/The_Lost_Hero.jpg'),
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9d/The_Lost_Hero_210.jpg/220px-The_Lost_Hero_210.jpg',
         language: "Inglés",
         publisher: "Disney-Hyperion"
     },
@@ -15,7 +15,7 @@ export const ItemDetails = [
         title: "Mistborn",
         author: "Brandon Sanderson",
         price: 800,
-        img: require('../Imagenes/Mistborn_Trilogy.jpg'),
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/44/Mistborn-cover.jpg/220px-Mistborn-cover.jpg',
         language: "Inglés",
         publisher: "Tor Books"
     },
@@ -25,7 +25,7 @@ export const ItemDetails = [
         title: "Stormfront",
         author: "Jim Butcher",
         price: 700,
-        img: require('../Imagenes/Storm_Front.jpg'),
+        img: 'https://upload.wikimedia.org/wikipedia/en/2/2b/Storm_Front.png',
         language: "Inglés",
         publisher: "Penguin Books"
     },
@@ -35,7 +35,7 @@ export const ItemDetails = [
         title: "Inkheart",
         author: "Cornelia Funke",
         price: 900,
-        img: require('../Imagenes/Inkheart.jpg'),
+        img: 'https://corneliafunke.com/phpThumb/phpThumb.php?src=/content/buecher/51/cover_en.png&w=140&f=png&sia=inkheart',
         language: "Inglés",
         publisher: "Scholastic"
     },
@@ -45,7 +45,7 @@ export const ItemDetails = [
         title: "Six of Crows",
         author: "Leigh Bardugo",
         price: 1000,
-        img: require('../Imagenes/Six_Of_Crows.jpg'),
+        img: 'https://www.leighbardugo.com/wp-content/uploads/2018/04/six-crows.jpg',
         language: "Inglés",
         publisher: "Henry Holt and Co"
     },
@@ -55,7 +55,7 @@ export const ItemDetails = [
         title: "The Raven Boys",
         author: "Maggie Stiefvater",
         price: 750,
-        img: require('../Imagenes/The_Raven_Cycle.jpg'),
+        img: 'http://www.maggiestiefvater.com/wp-content/themes/maggiestiefvater%20-%20pods%202.0/images/raven-cycle/ravenboys-cover.jpg',
         language: "Inglés",
         publisher: "Scholastic"
     }
