3c8c77760afdd6eb6b61b77afcc9788a4056678d
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-24 20:22:28 -0300

    Sección Home de la página

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BestBook from '../BestBook/BestBook';
+import Header from '../Header/Header';
+import SearchBar from '../SearchBar/SearchBar';
+
+function Home(){
+    return(
+        <>
+        <>
+        <Header />
+        </>
+        <>
+        <SearchBar />
+        </>
+        <>
+        <BestBook />
+        </>
+        </>
+    )
+}
+
+export default Home; 
+
