74ed0752124f250db3ce392c887f9e8291d5d12b
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-24 20:24:06 -0300

    Modifiqué los links

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,19 +2,22 @@ import React, {Component} from 'react';
 import { MenuItems } from './MenuItems';
 import CartWidget from '../CartWidget/CartWidget';
 import './NavBar.css';
+import {Link} from 'react-router-dom';
 
 class NavBar extends Component {
     render() {
         return(
             <nav className="NavBarItems">
+                <Link to="/home">
                 <h1 className="navbar-logo" id="titulo"><i className="fas fa-cloud"></i>En la Nube</h1>  
+                </Link>
             <ul className="nav-menu">
                 {MenuItems.map((item, index) => {
                     return(
                         <li key={index}>
-                        <a className={item.cName} href={item.url}>
+                        <Link className={item.cName} to={item.url}>
                             {item.title}
-                        </a>
+                        </Link>
                         </li>
                     )
                 })}
