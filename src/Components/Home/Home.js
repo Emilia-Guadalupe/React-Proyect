a0f95ed034b23334934376906c0593abf47e0807
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-25 20:00:59 -0300

    Sumé ItemList al componente Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import BestBook from '../BestBook/BestBook';
 import Header from '../Header/Header';
 import SearchBar from '../SearchBar/SearchBar';
+import ItemListContainer from '../ItemListContainer/ItemListContainer';
+import './Home.css';
 
 function Home(){
     return(
@@ -15,6 +17,10 @@ function Home(){
         <>
         <BestBook />
         </>
+        <div id="books">
+        <h3 id="itemList">Nuestros libros:</h3>
+        <ItemListContainer />
+        </div>
         </>
     )
 }
