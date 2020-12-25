7078f32fcd98383601fe14fc32c8bba78ca76c64
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-25 19:59:32 -0300

    Completé el contenido del componente

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './about.css';
 
 function About(){
     return(
-        <div>
-            <p>Sobre nosotros</p>
+        <div id="aboutContainer">
+        <div id="one">
+            <img id="library" src="https://static01.nyt.com/images/2019/12/17/books/review/17fatbooks/17fatbooks-jumbo.jpg?quality=90&auto=webp" alt="librería"></img>
+        </div>
+        <div id="two">
+            <h2 id="title">Sobre nosotros:</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
         </div>
     )
 }
