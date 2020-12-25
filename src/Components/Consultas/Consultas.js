5ad2e0d606a189786e9a649cb2792cf3108c51b5
Emilia <m.emilia.guadalupe@gmail.com>
2020-12-25 20:00:21 -0300

    Completé el componente Consultas

--- src/Components/Consultas/Consultas.js
+++ src/Components/Consultas/Consultas.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './consultas.css';
 
 function Consultas(){
     return(
+        <div id="questionsContainer">
+        <div id="twoPart">
+            <h5 className="questions">Costos de envío:</h5>
+            <p className="questions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
         <div>
-            <p>Consultas</p>
+            <h5 className="questions">Nuestro radio de entrega:</h5>
+            <p className="questions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div>
+            <h5 className="questions">Tiempos de entrega:</h5>
+            <p className="questions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div>
+            <p className="questions">¿Tenés alguna otra consulta?</p>
+            <p className="questions">Escribinos a maildeprueba@maildeprueba.com</p>
+        </div>
         </div>
     )
 }
