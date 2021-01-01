824e4a8e7a215f61980dd22cf7797d95ad7bb188
Emilia <m.emilia.guadalupe@gmail.com>
2021-01-01 20:38:56 -0300

    Container del Carrito

---
+++ src/Components/CartContainer/CartContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CartContainer() {
+    return(
+        <>
+        <div>
+            <p>Soy el Container del Carrito</p>
+        </div>
+        </>
+    )
+}
+
+export default CartContainer; 
