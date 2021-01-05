86c4e019349309b70e7af9faac058ed5f81de280
Emilia <m.emilia.guadalupe@gmail.com>
2021-01-05 15:09:49 -0300

    Agregué el stock detallado de cada producto

--- src/Components/ItemDetails/Item.js
+++ src/Components/ItemDetails/Item.js
@@ -10,7 +10,8 @@ export const productDetails = [
         "publisher": "Disney-Hyperion",
         "year": 2010,
         "pages": 557,
-        "abstract": "Tres semidioses. Una profecía. Y solo cuatro días para lograr lo imposible. Adéntrate en el mundo de «Los héroes del Olimpo», la atrapante serie de aventuras fantásticas de Rick Riordan."
+        "abstract": "Tres semidioses. Una profecía. Y solo cuatro días para lograr lo imposible. Adéntrate en el mundo de «Los héroes del Olimpo», la atrapante serie de aventuras fantásticas de Rick Riordan.",
+        "stock": 10
     },
     
     {
@@ -23,7 +24,8 @@ export const productDetails = [
         "publisher": "Tor Books",
         "year": 2006,
         "pages": 541,
-        "abstract": "El imperio final inicia la saga «Nacidos de la Bruma [Mistborn]», obra imprescindible del Cosmere, el universo destinado a dar forma a la serie más extensa y fascinante jamás escrita en el ámbito de la fantasía épica."
+        "abstract": "El imperio final inicia la saga «Nacidos de la Bruma [Mistborn]», obra imprescindible del Cosmere, el universo destinado a dar forma a la serie más extensa y fascinante jamás escrita en el ámbito de la fantasía épica.",
+        "stock": 5
     },
     
     {
@@ -36,7 +38,8 @@ export const productDetails = [
         "publisher": "Penguin Books",
         "year": 1990, 
         "pages": 322,
-        "abstract": "Harry Dresden se ha convertido en una ayuda insustituible para la policía de Chicago, porque es el único a quien pueden acudir cuando una investigación entra de lleno en lo paranormal. Es el caso de un doble asesinato con tintes de magia negra. Y detrás de la magia negra siempre hay un mago oscuro."
+        "abstract": "Harry Dresden se ha convertido en una ayuda insustituible para la policía de Chicago, porque es el único a quien pueden acudir cuando una investigación entra de lleno en lo paranormal. Es el caso de un doble asesinato con tintes de magia negra. Y detrás de la magia negra siempre hay un mago oscuro.",
+        "stock": 7
     },
     
     {
@@ -49,7 +52,8 @@ export const productDetails = [
         "publisher": "Scholastic",
         "year": 2003,
         "pages": 534,
-        "abstract": "Esta nueva novela de Cornelia Funke es magia, es mágica y es fantástica: es un viaje al mundo de los libros, una gran novela de aventuras y una declaración de amor a los grandes textos universales que cautivan a los lectores. Y también, una lucha feroz entre ficción y realidad, y entre bien y mal."
+        "abstract": "Esta nueva novela de Cornelia Funke es magia, es mágica y es fantástica: es un viaje al mundo de los libros, una gran novela de aventuras y una declaración de amor a los grandes textos universales que cautivan a los lectores. Y también, una lucha feroz entre ficción y realidad, y entre bien y mal.",
+        "stock": 12
     },
     
     {
@@ -62,7 +66,8 @@ export const productDetails = [
         "publisher": "Henry Holt and Co",
         "year": 2017,
         "pages": 465,
-        "abstract": "Ketterdam: Un desbordante foco de comercio internacional donde todo se puede conseguir por el precio adecuado, como bien sabe el prodigio criminal Kaz Brekker. A Kaz le acaban de ofrecer la oportunidad de llevar a cabo un gran robo, un arriesgado golpe que podría hacerle más rico de lo que se jamás se ha atrevido a imaginar en sus sueños más salvajes. Pero no podrá llevarlo a cabo por sí solo: tendrá que reclutar a un peculiar equipo formado por un convicto, una espía, un pistolero, una Grisha, un ladrón y un fugitivo."
+        "abstract": "Ketterdam: Un desbordante foco de comercio internacional donde todo se puede conseguir por el precio adecuado, como bien sabe el prodigio criminal Kaz Brekker. A Kaz le acaban de ofrecer la oportunidad de llevar a cabo un gran robo, un arriesgado golpe que podría hacerle más rico de lo que se jamás se ha atrevido a imaginar en sus sueños más salvajes. Pero no podrá llevarlo a cabo por sí solo: tendrá que reclutar a un peculiar equipo formado por un convicto, una espía, un pistolero, una Grisha, un ladrón y un fugitivo.",
+        "stock": 4
     },
     
     {
@@ -75,6 +80,7 @@ export const productDetails = [
         "publisher": "Scholastic",
         "year": 2013,
         "pages": 409,
-        abstract: "Cada año, la noche de San Marcos, Blue Sargent acompaña a su madre al camino de los espíritus para ver a los que morirán en los siguientes doce meses. Sin embargo, este año es distinto: Blue, la única de su familia que no tiene facultades adivinatorias, ha visto a uno de estos espíritus, y eso solo puede significar que es su amor verdadero o que lo va a matar. Él se llama Gansey y es el estudiante más rico del colegio privado más elitista de la zona, la Academia Aglionby, cuyo emblema es un cuervo que todos los estudiantes llevan bordado en el jersey. Junto a Adam, un estudiante brillante, celoso del poder económico de sus compañeros; Ronan, un chico con problemas emocionales desde la muerte de su padre, y Noah, el observador taciturno que apenas habla, forman los chicos del cuervo, y están empeñados en descubrir la ubicación de la tumba del último rey galés, Glendower, el Rey Cuervo."
+        abstract: "Cada año, la noche de San Marcos, Blue Sargent acompaña a su madre al camino de los espíritus para ver a los que morirán en los siguientes doce meses. Sin embargo, este año es distinto: Blue, la única de su familia que no tiene facultades adivinatorias, ha visto a uno de estos espíritus, y eso solo puede significar que es su amor verdadero o que lo va a matar. Él se llama Gansey y es el estudiante más rico del colegio privado más elitista de la zona, la Academia Aglionby, cuyo emblema es un cuervo que todos los estudiantes llevan bordado en el jersey. Junto a Adam, un estudiante brillante, celoso del poder económico de sus compañeros; Ronan, un chico con problemas emocionales desde la muerte de su padre, y Noah, el observador taciturno que apenas habla, forman los chicos del cuervo, y están empeñados en descubrir la ubicación de la tumba del último rey galés, Glendower, el Rey Cuervo.",
+        "stock": 9
     }
 ]
