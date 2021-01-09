a04c77b33c5675d4a13b780327e094c381a21e1e
Emilia <m.emilia.guadalupe@gmail.com>
2021-01-09 19:08:08 -0300

    Agregué el ProductContext y el CartContext

--- src/App.js
+++ src/App.js
@@ -10,15 +10,19 @@ import ItemDetailContainer from './Components/ItemDetails/ItemDetailContainer';
 import ItemDetail from './Components/ItemDetails/ItemDetail';
 import CartContainer from './Components/CartContainer/CartContainer';
 import ItemList from './Components/ItemDetails/ItemList';
+import {ProductContextProvider} from './Components/Context/ProductContext';
+import  {CartContextProvider} from './Components/Context/CartContext';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+    <ProductContextProvider>
     <div className="App">
     <>
       <NavBar />
     </>
+    <CartContextProvider>
     <Switch>
       <Route exact path="/">
       <Home />
@@ -45,10 +49,12 @@ function App() {
       <CartContainer />
       </Route>
     </Switch>
+    </CartContextProvider>
     <div>
       <Footer />
     </div>
     </div>
+    </ProductContextProvider>
     </BrowserRouter>
   );
 }
