5ccf4d1e151261d17fec83f30424f35ac424f909
Emilia <m.emilia.guadalupe@gmail.com>
2021-01-09 19:08:29 -0300

    Sumé la información tomada desde el CartContext

--- src/Components/CartContainer/CartContainer.js
+++ src/Components/CartContainer/CartContainer.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import CartContext from '../Context/CartContext';
 
 function CartContainer() {
+    const { quantity, productId, productPrice, productTitle } = useContext(CartContext);
+
     return(
         <>
         <div>
-            <p>Soy el Container del Carrito</p>
+            <h3>Tu carrito de compras: </h3>
+            <p>Id de tu producto: {productId}</p>
+            <p>La cantidad seleccionada es: {quantity}</p>
+            <p>El título del libro es: ${productPrice}</p>
+            <p>El precio del producto es: {productTitle}</p>
         </div>
         </>
     )
