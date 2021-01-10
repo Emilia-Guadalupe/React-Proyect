982aa7fb627ec9e72ba403bc2b01634e84912631
Emilia <m.emilia.guadalupe@gmail.com>
2021-01-10 19:03:31 -0300

    Modifiqué la función add para enviar los datos al carrito

--- src/Components/ItemCount/AddToCart.js
+++ src/Components/ItemCount/AddToCart.js
@@ -5,18 +5,21 @@ import './itemCount.css';
 
 function Toggle({data: {id, number, price, title}}){
     const [showing, setShowing] = useState(false);
-    const {getIformationFromApp} = useContext(CartContext);
+    const { setQuantity, setPrice, setTitle, setProductId } = useContext(CartContext);
 
     function add() {
-        setShowing({ showing: !showing })
-        console.log(`El numero de productos es ${number} y el id es: ${id}`);
-}
+        setShowing({ showing: !showing });
+        setQuantity(number);
+        setPrice(price);
+        setTitle(title);
+        setProductId(id);
+    }
 
     return(
     <>
     <div>
     { showing
-    ? <Link onClick={getIformationFromApp(id, number, price, title)} to="/carrito"><button className="cart">Terminar Compra</button></Link>
+    ? <Link to="/carrito"><button className="cart">Terminar Compra</button></Link>
     : <button className="cart" onClick={add}>Agregar al Carrito</button>
     }
     </div>
