539b61f58d5aa0a0ffc46649ebd44927ca676e7c
Emilia <m.emilia.guadalupe@gmail.com>
2021-01-25 08:44:50 -0300

    Modifiqué el archivo Firebase

--- src/FirebaseSettings.js
+++ src/FirebaseSettings.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "@firebase/auth";
 import "@firebase/firestore";
 
-const apiKey = process.env.REACT_APP_apiKey
+/*const apiKey = process.env.REACT_APP_apiKey
 const authDomain = process.env.REACT_APP_authDomain
 const databaseURL = process.env.REACT_APP_databaseURL
 const projectId = process.env.REACT_APP_projectId
@@ -10,7 +10,17 @@ const storageBucket = process.env.REACT_APP_storageBucket
 const messagingSenderId = process.env.REACT_APP_messagingSenderId
 const appId = process.env.REACT_APP_appId
 
-const firebaseConfig = { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId };
+const firebaseConfig = { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId }; */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtL0T62kvmWzbGDvnLbeS0h5ruU4tgYCE",
+    authDomain: "proyecto-react-coderhous-91c2d.firebaseapp.com",
+    databaseURL: "https://proyecto-react-coderhous-91c2d-default-rtdb.firebaseio.com",
+    projectId: "proyecto-react-coderhous-91c2d",
+    storageBucket: "proyecto-react-coderhous-91c2d.appspot.com",
+    messagingSenderId: "705509549115",
+    appId: "1:705509549115:web:574e8bbec9a982cd2b681e"
+    };
 
 const app = firebase.initializeApp(firebaseConfig);
 
