c67e9294718a480827d081a623c633b5bcd173df
Emilia <m.emilia.guadalupe@gmail.com>
2021-01-30 09:46:19 -0300

    Formateando el Forms

--- src/Components/CartForms/CartForms.js
+++ src/Components/CartForms/CartForms.js
@@ -4,7 +4,7 @@ import './formStyles.css';
 
 function CartForms() {
 
-    const { setName, setEmail, setPhone, manejarCompra, name, email, phone } = useContext(CartContext);
+    const { setName, setEmail, setPhone, manejarCompra, name, email, phone, compra, thanks } = useContext(CartContext);
 
     function onNameChange(evt) {
         setName(evt.target.value);
@@ -23,25 +23,21 @@ function CartForms() {
         <div className="formContainer">
             <div className="explanation">
                 <h4 id="title"> ¡Confirmá tu compra! </h4>
-                <p id="description">Si ya tenés todo listo, confirmá tu compra para ser redirigido a Mercado Pago para abonar tu compra</p>
+                <p id="description">Si ya tenés todo listo, confirmá tu compra</p>
             </div>
             <div>
-                <form className="forms">
-                <>
+                <form className="forms" onSubmit={manejarCompra}>
                 <label id="name-label"> Nombre Completo: </label>
                 <input required placeholder="Ingresá tu nombre" type="text" id="name" className="datos" onChange={evt => onNameChange(evt)} />
-                </>
-                <>
                 <label id="email-label">Tu email:</label>
                 <input type="email" id="email" name="email" placeholder="Ingresá tu email" className="datos" required onChange={evt => onEmailChange(evt)}></input>
-                </>
-                <>
                 <label id="number-label">Tu teléfono:</label>
                 <input  placeholder="Ingresá tu teléfono" required type="number" id="number" className="datos" onChange={evt => onPhoneChange(evt)}></input>
-                </>
-                <button type="submit" className="submitButton" onClick={manejarCompra} disabled={!(name !== "" && phone !== "" && email !== "")}>Enviar</button>
+                
+                <button type="submit" className="submitButton" disabled={!(name !== "" && phone !== "" && email !== "")}>Enviar</button>
                 </form>
             </div>
+            <div>{thanks}</div>
         </div>
     )
 }
