7aced1dd7bac1bdf8109fbd4876e5dbeb8d6661a
Emilia <m.emilia.guadalupe@gmail.com>
2021-01-31 19:56:47 -0300

    Configurando confirmación de compra :D

--- src/Components/CartForms/CartForms.js
+++ src/Components/CartForms/CartForms.js
@@ -4,7 +4,9 @@ import './formStyles.css';
 
 function CartForms() {
 
-    const { setName, setEmail, setPhone, manejarCompra, name, email, phone, setConfirmEmail, thanks, confirmEmail } = useContext(CartContext);
+    const { setName, setEmail, setPhone, manejarCompra, name, email, phone, setConfirmEmail, thanks, confirmEmail, compra } = useContext(CartContext);
+
+    console.log(compra);
 
     function onNameChange(evt) {
         setName(evt.target.value);
@@ -31,19 +33,33 @@ function CartForms() {
             </div>
             <div>
                 <form className="forms" onSubmit={manejarCompra}>
+
                 <label id="name-label"> Nombre Completo: </label>
                 <input required placeholder="Ingresá tu nombre" type="text" id="name" className="datos" onChange={evt => onNameChange(evt)} />
+                
                 <label id="email-label">Tu email:</label>
                 <input type="email" id="email" name="email" placeholder="Ingresá tu email" className="datos" required onChange={evt => onEmailChange(evt)}></input>
+                
                 <label id="secondond-email-label">Reingresá tu email:</label>
                 <input type="email" id="SecondEmail" name="email" placeholder="Ingresá nuevamente tu email" className="datos" required onChange={evt => secondOnEmailChange(evt)}></input>
+                
                 <label id="number-label">Tu teléfono:</label>
                 <input  placeholder="Ingresá tu teléfono" required type="number" id="number" className="datos" onChange={evt => onPhoneChange(evt)}></input>
                 
                 <button type="submit" className="submitButton" disabled={!(name !== "" && phone !== "" && email !== "" && confirmEmail !== "")}>Enviar</button>
                 </form>
             </div>
-            <div>{thanks}</div>
+            <div>
+                {compra ? (
+                    <>
+                    <p>
+                    ¡Muchas gracias, {compra.buyer.name}! 
+                    </p>
+                    <p>El id de tu compra es: {compra.id}</p>
+                    <p>Total de tu compra: ${compra.total}</p>
+                    </>
+                ) : null }
+            </div>
         </div>
     )
 }
